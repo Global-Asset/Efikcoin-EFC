0056467ffc089c89ade99557237613805f041bf8
Efikcoin wallet <Support@efikcoin.xyz>
2026-07-31 05:13:51 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('efc-pay-v1').then((cache) => cache.addAll(['/index.html', '/manifest.json'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
